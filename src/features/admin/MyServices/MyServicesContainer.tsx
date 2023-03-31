61d63d917d97a3e3b91d5bfa1306d6e85e6e21ef
val2cak <valentina.tucak99@gmail.com>
2023-03-31 11:13:53 +0200

    adding pagination to my services

--- src/features/admin/MyServices/MyServicesContainer.tsx
+++ src/features/admin/MyServices/MyServicesContainer.tsx
@@ -5,13 +5,28 @@ import arrow from '../../../assets/shapes/arrow-right-light.png';
 import { useGetServicesListQuery } from '../../guest/ServicesList/servicesApiSlice';
 import { getUserFromStorage } from '../../../services/storage';
 import { User } from '../../../types/typeDefinitions';
+import { Pagination } from '@mui/material';
+import { useState } from 'react';
 
 const ServicesListContainer = () => {
   const userJson: string | null = getUserFromStorage();
   const user: User | null = userJson ? JSON.parse(userJson).user : null;
 
-  const { data: servicesListData, isFetching: isServicesListDataLoading } =
-    useGetServicesListQuery({ authorId: user.id, page: 1, pageSize: 8 });
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const handlePageChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setCurrentPage(value);
+    refetch();
+  };
+
+  const {
+    data: servicesListData,
+    isFetching: isServicesListDataLoading,
+    refetch,
+  } = useGetServicesListQuery({ page: currentPage, pageSize: 8 });
 
   const servicesData = servicesListData?.services;
 
@@ -27,39 +42,31 @@ const ServicesListContainer = () => {
         </div>
       </div>
 
-      {!isServicesListDataLoading && (
-        <div className='flex flex-wrap flex-start gap-4 items-center flex-row px-56 w-full'>
-          {servicesData.map((item, index) => (
-            <ServiceCard
-              id={item.id}
-              key={index}
-              author={item.author}
-              title={item.title}
-              date={item.date}
-              price={item.price}
-              location={item.location}
-              description={item.description}
-              people={item.people}
-            />
-          ))}
-        </div>
-      )}
+      <div className='flex flex-col px-56 gap-8 pb-8'>
+        {!isServicesListDataLoading && (
+          <div className='flex flex-wrap flex-start gap-4 items-center flex-row  w-full'>
+            {servicesData.map((item, index) => (
+              <ServiceCard
+                id={item.id}
+                key={index}
+                author={item.author}
+                title={item.title}
+                date={item.date}
+                price={item.price}
+                location={item.location}
+                description={item.description}
+                people={item.people}
+              />
+            ))}
+          </div>
+        )}
 
-      <div className='w-full flex justify-center gap-16 p-8'>
-        <button className='transition ease-in-out delay-150 hover:-translate-x-4 duration-300'>
-          <img
-            src={arrow}
-            alt='arrow left'
-            className='object-fill h-18 w-36 -rotate-180'
-          />
-        </button>
-        <button className='transition ease-in-out delay-150 hover:translate-x-4 duration-300'>
-          <img
-            src={arrow}
-            alt='arrow right'
-            className='object-fill h-18 w-36'
-          />
-        </button>
+        <Pagination
+          count={servicesListData?.totalPages ?? 1}
+          size='large'
+          page={currentPage}
+          onChange={handlePageChange}
+        />
       </div>
     </main>
   );
