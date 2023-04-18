e2f4d2d6c725f3994e30cad5ec4708f1bec0309a
val2cak <valentina.tucak99@gmail.com>
2023-04-18 16:08:35 +0200

    styling photo upload modal

--- src/components/UploadImage/UploadImage.tsx
+++ src/components/UploadImage/UploadImage.tsx
@@ -15,13 +15,33 @@ const UploadImage: FC<Props> = ({ onSave }) => {
   const handleFileUpload = () => {
     const pickerOptions: PickerOptions = {
       accept: ['image/*'],
-      fromSources: ['local_file_system', 'url'],
+      fromSources: ['local_file_system', 'webcam', 'url'],
       transformations: {
         crop: {
           aspectRatio: 1,
           force: true,
         },
+        rotate: true,
+        circle: true,
       },
+      customText: {
+        Save: 'Spremi',
+        Reset: 'Poništi',
+        Crop: 'Izreži',
+        Circle: 'Zaokruži',
+        Rotate: 'Rotiraj',
+        'Select Files to Upload': 'Odaberite datoteku za prijenos',
+        'or Drag and Drop, Copy and Paste Files':
+          'ili povucite i ispustite, kopirajte i zalijepite datoteku',
+        'Enter a URL': 'Unesite URL',
+        'My Device': 'Moje računalo',
+        'Take Photo': 'Snimi fotografiju',
+        'Webcam Disabled': 'Kamera onemogućena',
+        'Please enable your webcam to take a photo.':
+          'Omogućite svoju web kameru za snimanje fotografije.',
+      },
+      allowManualRetry: true,
+
       onUploadDone: (res) => {
         onSave(res.filesUploaded[0]?.url);
       },
