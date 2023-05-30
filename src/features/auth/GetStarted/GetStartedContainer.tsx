d5fbb2c311ffbc94583951fdda0b9d17c2034285
val2cak <valentina.tucak99@gmail.com>
2023-05-30 19:26:06 +0200

    temporary removing google login

--- src/features/auth/GetStarted/GetStartedContainer.tsx
+++ src/features/auth/GetStarted/GetStartedContainer.tsx
@@ -13,7 +13,7 @@ const GetStartedContainer = () => {
       </h1>
 
       <div className='flex flex-row sm:px-0 lg:px-16 px-5 2xl:py-24 justify-center items-center sm:h-full sm:bg-getStarted sm:bg-no-repeat sm:bg-top-right'>
-        <div className='flex flex-col sm:w-full w-1/3 gap-8 justify-center items-center sm:py-8 sm:bg-grayColor sm:bg-opacity-50'>
+        <div className='flex flex-col sm:w-full w-1/3 gap-8 justify-center items-center sm:py-12 sm:bg-grayColor sm:bg-opacity-50'>
           <button
             className='button bg-secondaryColor text-lightColor h-[70px]'
             onClick={() => navigateTo('/auth/login')}
@@ -27,13 +27,13 @@ const GetStartedContainer = () => {
             Registracija
           </button>
 
-          <div className='flex flex-row text-lightColor items-center gap-1 pt-8'>
+          {/* <div className='flex flex-row text-lightColor items-center gap-1 pt-8'>
             <hr />
             <p>ili se registriraj s</p>
             <hr />
           </div>
 
-          <GoogleLoginComponent />
+          <GoogleLoginComponent /> */}
         </div>
 
         <div className='flex justify-center items-center sm:hidden'>
